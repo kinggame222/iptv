850eabf0e99134310222b8c94d779dc5266bead7
kinggame222 <48651883+kinggame222@users.noreply.github.com>
2022-04-01 10:37:43 -0400

    page yt

---
+++ script_js/script_video_yt.js
@@ -0,0 +1,57 @@
+
+//var video = {
+// "Video": [
+//     "https://www.youtube.com/embed/HuFuTbAxw9o",
+//     "https://www.youtube.com/embed/XweA9gU-gqo",
+
+// ]
+
+
+//}
+
+
+function d() {
+
+    
+    function replace1() {
+        var str1 = document.getElementById("NB_video2").value;
+        var newstr1 = str1.replace("https://www.youtube.com/watch?v=", "/");
+        console.log(newstr1);
+        return newstr1;
+    }
+
+
+    // Parse any JSON previously stored in allEntries
+    var existingEntries = JSON.parse(localStorage.getItem("allEntries"));
+    if (existingEntries == null) existingEntries = [];
+    var entryTitle = document.getElementById("NB_video").value;
+    var entryText = replace1();
+    var entry = {
+        "title": entryTitle,
+        "video": entryText
+    };
+    localStorage.setItem("base", JSON.stringify(entry));
+    // Save allEntries back to local storage
+    existingEntries.push(entry);
+
+    localStorage.setItem("allEntries", JSON.stringify(existingEntries));
+    var x = localStorage.getItem("allEntries");
+    console.log(localStorage.getItem("allEntries"));
+
+    let c = " ";
+
+    c = "https:www.youtube.com/embed" + existingEntries[entryTitle].video + "?autoplay=1";
+    console.log(c);
+    document.getElementById("vid").src = c;
+
+
+};
+
+function effacer_video(){
+    document.getElementById("vid").src = " "
+}
+
+
+
+
+
